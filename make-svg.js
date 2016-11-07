339999e4ece6df256771b9632cc2efd8e22217f1
bagrounds <bagrounds@gmail.com>
2016-11-06 19:35:18 -0800

    [PATCH][CHANGE] refactor generic svg creation function out of circle function

--- make-svg.js
+++ make-svg.js
@@ -25,33 +25,51 @@
    * @param {Number} options.y y position for center of circle
    * @param {Number} options.r radius of circle
    *
-   * @return {String} svg as a string
+   * @return {String} svg circle element as a string
    */
   function circle (options) {
-    var id = options.id
-    var x = options.x
-    var y = options.y
-    var r = options.r
+    var svgOptions = {
+      tag: 'circle',
+      attributes: {}
+    }
+
+    svgOptions.attributes.cx = options.x
+    svgOptions.attributes.cy = options.y
+    svgOptions.attributes.r = options.r
+
+    if (options.id) {
+      svgOptions.attributes.id = options.id
+    }
+
+    return svg(svgOptions)
+  }
+
+  /**
+   * Make an svg element.
+   *
+   * @function svg
+   *
+   * @param {Object} options all function parameters
+   * @param {String} options.tag svg element type
+   * @param {Object} options.attributes for the svg element
+   *
+   * @return {String} svg as a string
+   */
+  function svg (options) {
+    var attributes = options.attributes
+    var tag = options.tag
 
     var xmlObject = {
-      svg: [
-        {
-          circle: [
-            {
-              _attr: {
-                cx: x,
-                cy: y,
-                r: r
-              }
-            }
-          ]
-        }
-      ]
+      svg: []
     }
 
-    if (id) {
-      xmlObject.svg[0].circle[0]._attr.id = id
-    }
+    var element = {}
+
+    element[tag] = {}
+
+    element[tag]._attr = attributes
+
+    xmlObject.svg.push(element)
 
     return xml(xmlObject)
   }
